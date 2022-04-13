f74747708a5aa2d2ec5e11b48c24c8d66db1b092
Jiangqiu Shen <ivy22233qiu@live.com>
2022-04-13 16:25:58 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #first install oh-my-sh
+rm -rf ~/.oh-my-zsh ~/.zshrc
+yay -S git python python-pip wget
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #second, add plugin and theme
@@ -7,7 +9,7 @@ sed -i 's/^plugins.*/plugins=('"$plugins"')/g' ~/.zshrc
 sed -i 's/^ZSH_THEME.*/ZSH_THEME="af-magic"/g' ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 pip install thefuck --user
-git clone git://github.com/wting/autojump.git
+git clone https://github.com/wting/autojump.git
 cd autojump
 ./install.py
 cd ..
